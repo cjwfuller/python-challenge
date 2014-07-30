83852a5313b06d099a2b2a10e704cb7662b7e904
Chris Fuller <chris@cjwfuller.com>
2014-07-30 20:09:05 +0100

    solve level 2 but without string.maketrans

---
+++ level2.py
@@ -0,0 +1,11 @@
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+riddle = list("g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj.")
+
+num_letters = 26
+for idx, char in enumerate(riddle):
+    if(riddle[idx] in alphabet):
+        posn = alphabet.index(riddle[idx]) + 2
+        if(posn >= num_letters):
+            posn = posn - num_letters
+        riddle[idx] = alphabet[posn]
+print ''.join(riddle)
