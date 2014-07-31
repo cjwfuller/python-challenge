7dac789162a36c138eb36e85fa40baae43cca51f
Chris Fuller <chris@cjwfuller.com>
2014-07-31 19:34:29 +0100

    add solution comment to level 2

--- level2.py
+++ level2.py
@@ -1,3 +1,5 @@
+# Solution: Change 'map' to 'ocr' in the URL
+
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 riddle = list("g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj.")
 
